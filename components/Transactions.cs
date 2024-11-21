4b27ae1978d091dbb8250f6e7b745c338920ad10
Rose Mary Maputol <rosemarymaputol21@gmail.com>
2024-11-21 23:57:58 +0800

    Added transactions user form

---
+++ components/Transactions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace event_management_system.components
+{
+    public partial class Transactions : UserControl
+    {
+        public Transactions()
+        {
+            InitializeComponent();
+        }
+
+        private void iconPictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
